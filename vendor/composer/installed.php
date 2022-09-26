72eb02740a06b4caef2a623710e2f74f5a6294bb
docrot18 <80437818+docrot18@users.noreply.github.com>
2022-09-26 11:53:39 +0900

    init

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f944195730b81240025440ab3bc9328d9020d08f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f944195730b81240025440ab3bc9328d9020d08f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
